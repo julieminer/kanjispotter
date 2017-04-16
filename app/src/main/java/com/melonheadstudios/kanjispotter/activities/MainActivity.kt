7a91f1054396609eabf55a86e4f6a9de450f9a06
Jacob Miner <jake@steamclock.com>
2017-04-16 15:26:09 -0700

    Add main activity

---
+++ app/src/main/java/com/melonheadstudios/kanjispotter/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.melonheadstudios.kanjispotter.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.melonheadstudios.kanjispotter.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
