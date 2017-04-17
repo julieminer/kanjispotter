8216d5cb34b01b7c60e16b88075a84c7fc997907
Jacob Miner <jake@steamclock.com>
2017-04-17 14:10:19 -0700

    Add fabric integration

--- app/src/main/java/com/melonheadstudios/kanjispotter/MainApplication.kt
+++ app/src/main/java/com/melonheadstudios/kanjispotter/MainApplication.kt
@@ -1,16 +1,17 @@
 package com.melonheadstudios.kanjispotter
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
 import com.melonheadstudios.kanjispotter.injection.AndroidModule
 import com.melonheadstudios.kanjispotter.injection.ApplicationComponent
 import com.melonheadstudios.kanjispotter.injection.DaggerApplicationComponent
+import io.fabric.sdk.android.Fabric
 
 /**
  * MainApplication
  * Created by jake on 2017-04-15, 9:40 AM
  */
 class MainApplication: Application() {
-    private val TAG = "MainApplication"
 
     companion object {
         //platformStatic allow access it from java code
@@ -21,6 +22,8 @@ class MainApplication: Application() {
         super.onCreate()
         graph = DaggerApplicationComponent.builder().androidModule(AndroidModule(this)).build()
         graph.inject(this)
+
+        Fabric.with(this, Crashlytics())
     }
 
     override fun onTerminate() {
