579f1c2080eec783c21471e3962eba3234689518
communistWatermelon <zyko101@gmail.com>
2021-09-20 19:52:44 -0700

    Add extension for showing fading edges on lazycolumn

--- app/src/main/java/com/melonheadstudios/kanjispotter/extensions/ModifierExtensions.kt
+++ app/src/main/java/com/melonheadstudios/kanjispotter/extensions/ModifierExtensions.kt
@@ -1,7 +1,11 @@
 package com.melonheadstudios.kanjispotter.extensions
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
@@ -9,6 +13,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.unit.Dp
 
@@ -109,5 +114,62 @@ fun Modifier.verticalFadingEdge(
             ),
             topLeft = Offset(x = 0f, y = size.height - bottomFadingEdgeStrength),
         )
+    }
+}
+
+@SuppressLint("UnnecessaryComposedModifier")
+fun Modifier.verticalFadingEdge(
+    scrollState: LazyListState,
+    length: Dp,
+    edgeColor: Color? = null,
+) = composed(
+    debugInspectorInfo {
+        name = "length"
+        value = length
+    }
+) {
+    val color = edgeColor ?: MaterialTheme.colors.surface
+    val height = remember { mutableStateOf(0) }
+
+    Modifier.onSizeChanged { size -> height.value = size.height }.drawWithContent {
+        if (scrollState.layoutInfo.totalItemsCount != 0) {
+            val lengthValue = length.toPx()
+            val itemSize = scrollState.layoutInfo.visibleItemsInfo.first().size
+            val totalItems = scrollState.layoutInfo.totalItemsCount
+            val totalSize = itemSize * totalItems
+            val scrolledTopPosition = ((scrollState.firstVisibleItemIndex) * itemSize) + scrollState.firstVisibleItemScrollOffset
+            val scrolledBottomPosition = scrolledTopPosition + height.value
+            val scrollFromBottom = totalSize - scrolledBottomPosition
+            val topFadingEdgeStrength = lengthValue * (scrolledTopPosition / lengthValue).coerceAtMost(1f)
+            val bottomFadingEdgeStrength = lengthValue * (scrollFromBottom / lengthValue).coerceAtMost(1f)
+
+            drawContent()
+            drawRect(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        color,
+                        Color.Transparent,
+                    ),
+                    startY = 0f,
+                    endY = topFadingEdgeStrength,
+                ),
+                size = Size(
+                    this.size.width,
+                    topFadingEdgeStrength
+                ),
+            )
+
+            drawRect(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        color,
+                    ),
+                    startY = size.height - bottomFadingEdgeStrength,
+                    endY = size.height,
+                ),
+                topLeft = Offset(x = 0f, y = size.height - bottomFadingEdgeStrength),
+            )
+        }
     }
 }
